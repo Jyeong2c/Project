c0751dd190bced375c53a39ad2cfc67a5e7f8bc4
jaehyeonjeong <definejae234@gmail.com>
2023-02-03 21:19:59 +0900

    mongoDB Json Parsing

--- JsonParsing/mainwindow.cpp
+++ JsonParsing/mainwindow.cpp
@@ -21,7 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     manager = new QNetworkAccessManager(this);
     //네트워크 요구 변수
-    req.setUrl(QUrl("http://192.168.0.12:4000/allImagelist?by=panorama1"));
+    //req.setUrl(QUrl("http://192.168.0.12:4000/allImagelist?by=panorama1"));
+    req.setUrl(QUrl("http://192.168.0.12:40000/api/patient/"));
 
     //웹페이지 수신 완료 신호를 받으면 QEventLoop의 quit() 메소드를 실행
     connect(manager, SIGNAL(finished(QNetworkReply *)), &connection_loop, SLOT(quit()));
@@ -41,8 +42,6 @@ void MainWindow::on_AcessMakeButton_clicked()
     rep = manager->get(req);
 
     connection_loop.exec();//동기화 부분
-    QByteArray data = rep->readAll(); //해당 Url 전체 Json데이터 읽기
-    if(data.isEmpty() == true) qDebug() << "Need to fill Json Data";
 
 #if 0
     //만일 Array부터 시작하는 경우
@@ -86,13 +85,13 @@ void MainWindow::on_AcessMakeButton_clicked()
     if(rep->error() == QNetworkReply::NoError){
         QJsonDocument doc1 = QJsonDocument::fromJson(data);
         if(doc1.isObject() == false) qDebug() << "It is not a Json object";
-        QJsonArray jsonArr  = doc1["array"].toArray();
+        QJsonArray jsonArr  = doc1["response"].toArray();
 
         QString decodeData = "";
         for(int i = 0; i < jsonArr.size(); i++){
-            QJsonObject jsonObj = jsonArr.at(i).toObject();
-            QJsonObject patientObj = jsonObj.constFind("patient")->toObject();
-            QString ID = patientObj["ID"].toString();
+            QJsonObject patientObj = jsonArr.at(i).toObject();
+            //QJsonObject patientObj = jsonObj.constFind("patient")->toObject();
+            QString ID = patientObj["_id"].toString();
             QString Name = patientObj["Name"].toString();
             int Age = patientObj["Age"].toInt();
             QString DoctorID = patientObj["DoctorID"].toString();
@@ -109,7 +108,7 @@ void MainWindow::on_AcessMakeButton_clicked()
         }
         ui->plainTextEdit->setPlainText(decodeData);
     }
-#else
+#elif 0
     //만일 Array부터 시작하는 경우
     QJsonParseError parseError;
     if(rep->error() == QNetworkReply::NoError){
@@ -138,13 +137,47 @@ void MainWindow::on_AcessMakeButton_clicked()
 
         ui->plainTextEdit->setPlainText(decodeData);
     }
+#else
+    if (rep->error( ) == QNetworkReply::NoError) {
+        QString strReply = (QString)rep->readAll( );
+        // Json 파싱
+        qDebug( ) << "Response:" << strReply;
 
+        QJsonDocument jsonResponse =
+                QJsonDocument::fromJson(strReply.toUtf8( ));
 
+        //QJsonArray jsonArr  = doc1["response"].toArray();
+
+        QString decodeData;
+        QJsonArray jsonArr = jsonResponse["response"].toArray();
+        for(int i = 0; i < jsonArr.size(); i++) {
+            QJsonObject patientObj = jsonArr.at(i).toObject();    //jsonResponse.object();
+#if 0
+            foreach(const QString& key, jsonObj.keys()) {
+                QJsonValue value = jsonObj.value(key);
+                qDebug() << "Key = " << key << ", Value = " << value.toString();
+            }
+#else
+            QString ID = patientObj["_id"].toString();
+            QString Name = patientObj["Name"].toString();
+            int Age = patientObj["Age"].toInt();
+            QString DoctorID = patientObj["DoctorID"].toString();
+            QString PhotoDate = patientObj["PhotoDate"].toString();
+            QString ImageListURL = patientObj["ImageListUrl"].toString();
+
+            decodeData.append("ID : " + ID  + "\n");
+            decodeData.append("Name : " + Name + "\n");
+            decodeData.append("Age : " + QString::number(Age) + "\n");
+            decodeData.append("DoctorID : " + DoctorID + "\n");
+            decodeData.append("PhotoDate : " + PhotoDate + "\n");
+            decodeData.append("ImageListUrl : " + ImageListURL + "\n\n");
+#endif
+        }
+        ui->plainTextEdit->setPlainText(decodeData);
+        delete rep;
+
+    }
 
 #endif
 
 }
-
-
-
-
